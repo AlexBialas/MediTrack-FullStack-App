efef8f3a5a7ffa6de88cbbaaa68cf3d090a83dc2
dci-student <aleksandrabialas94@gmail.com>
2025-05-21 10:17:53 +0200

    add basic routing with React Router and page components

--- meditrack-frontend/src/App.tsx
+++ meditrack-frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">hallo</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
